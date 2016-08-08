371e997a71ddf2ce26437fef53433cd438246b16
Wendell Sun <iwendellsun@gmail.com>
2016-08-08 23:17:01 +0800

    Init commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintf(w, uuid.NewV4().String())
+}
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
