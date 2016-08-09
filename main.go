01adaed894d3c7457f71b5d955033cd20c76657a
Wendell Sun <iwendellsun@gmail.com>
2016-08-09 13:28:18 +0800

    Test cache

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Fprintf(w, "uuid:"+uuid.NewV4().String())
+	fmt.Fprintf(w, "test cache:"+uuid.NewV4().String())
 }
 func main() {
 	http.HandleFunc("/", sayhelloName)
